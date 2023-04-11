9753aacc610f324233a7edb619e3608011c79da9
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-04-11 17:35:07 -0600

    Webpack&Babel

--- Lab8Babel/Lab7STW/modular/babel.config.js
+++ Lab8Babel/Lab7STW/modular/babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
     api.cache(true);
   
-    const presets = ["@babel/preset-env"];
+    const presets = [
+      [
+        "@babel/preset-env",{
+        "corejs":3.6,
+        "useBuiltIns":"usage",
+        "debug":true
+        }
+      ]
+    ];
     const plugins = ["@babel/plugin-proposal-throw-expressions"];
   
     return {
